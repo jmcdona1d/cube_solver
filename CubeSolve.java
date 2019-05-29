6e88cc1b732620ee7e7b3d6e2590cce80532e74f
James McDonald <15jgm4@queensu.ca>
2019-05-29 19:41:02 -0400

    Created seperate classes for cube objects and solving methods

---
+++ CubeSolve.java
@@ -0,0 +1,20 @@
+
+public class CubeSolve{
+
+	private Cube cube;
+	
+	public CubeSolve(Cube c) {
+		cube = c;
+	}
+	
+	public void solve() {//reorder/rename solve method names as more are added
+		
+		while (whiteCross() != true);
+	//	while(F2L()!= true);
+	//	while(OLL()!=true);
+	//	while(PLL()!=true);
+	//	while(finish()!=true);
+	}
+	
+	
+}

6e963cccf7be457babb06ac718327fb02bfc7bae
James McDonald <15jgm4@queensu.ca>
2019-10-20 21:57:00 -0400

    created data source and domain layers

---
+++ REST_Testing/src/IdGenerator.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class IdGenerator {
+	
+	private AtomicLong nextId = new AtomicLong(1);
+	
+	public Long getNextId() {
+		return nextId.getAndIncrement();
+	}
+}
---
+++ REST_Testing/src/OrderRepository.java
@@ -0,0 +1,10 @@
+
+public class OrderRepository extends InMemoryRepository<Order> {
+
+	protected void updateIfExists(Order original, Order updated) {
+		original.setDescription(updated.getDescription());
+		original.setCostInCents(updated.getCostInCents());
+		original.setComplete(updated.isComplete());
+	}
+
+}
